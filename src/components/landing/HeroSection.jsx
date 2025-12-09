f7e774e2f75d492743a9c8359e90f8241b9e419c
ShreyanshJain105 <shreyanshjain1206@gmail.com>
2025-12-09 23:09:22 +0530

    updated styling and text content

--- src/components/landing/HeroSection.jsx
+++ src/components/landing/HeroSection.jsx
@@ -9,6 +9,16 @@ const HeroSection = () => {
                             <span className="block">Share Files Encrypted with </span>
                             <span className="block text-purple-500">DriveShare</span>
                         </h1>
+                        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                            Upload, Manage, and share your files encrypted . Accesible anywhere, anytime.
+                        </p>
+
+                        <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+                            <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                                <button className="flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-500 hover:bg-purple-600 md:py-4 md:text-lg md:px-10 transition-all duration-200 shadow-lg hover:shadow-xl ">Get Started</button>
+                                <button className="flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-500 hover:bg-green-600 md:py-4 md:text-lg md:px-10 transition-all duration-200 shadow-lg hover:shadow-xl"> Sign In</button>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
